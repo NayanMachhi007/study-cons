f6da4edc9b28aedd188a96821a82b35a26f585eb
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-02-23 17:10:51 +0000

    new code updated now

--- js/banner.js
+++ js/banner.js
@@ -1,16 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 10,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
   },
 });
