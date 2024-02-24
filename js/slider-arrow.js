c66609f91d0d2efcc3ee1dd6df85258488532ee4
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-02-24 13:35:48 +0000

    new code updated now

--- js/slider-arrow.js
+++ js/slider-arrow.js
@@ -1,13 +1,7 @@
 let swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
+    type: "",
   },
   navigation: {
     nextEl: ".swiper-button-next",
