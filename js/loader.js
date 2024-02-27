e72429e1a4ac8d984a57ede19167ecffdf72cfc6
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-02-27 18:10:20 +0000

    new code updated now

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,18 @@
-// Loader Javascript
-let preloader = document.getElementById("loading");
-window.addEventListener("load", function () {
-  preloader.style.display = "none";
+// // Loader Javascript
+// let preloader = document.getElementById("loading");
+// window.addEventListener(
+//   "load",
+//   function () {
+//     preloader.style.display = "none";
+//   },
+//   8000
+// );
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("loading").style.display = "none";
+  }, 5000); // 5000 milliseconds = 5 seconds
 });
